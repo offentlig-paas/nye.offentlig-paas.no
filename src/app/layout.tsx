33d6db9681ea36f032a1e14472fd1cb2db6a7536
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-05-30 10:57:17 +0200

    Legge til analytics og speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { type Metadata } from 'next'
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import '@/styles/tailwind.css'
 
@@ -40,6 +42,8 @@ export default function RootLayout({
             <Layout>{children}</Layout>
           </div>
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
