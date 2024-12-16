06aed21410a524abd13cc88291223efff315fe9b
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2024-12-16 20:31:36 +0100

    Add images and layout to årsbertening

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import Link from 'next/link';
 import React from 'react';
 
 export function BlueskyIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
@@ -54,5 +56,29 @@ export function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
       <path fillRule="evenodd" d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z" fill="currentColor" />
     </svg>
+  )
+}
+
+export function SocialLink({
+  className,
+  href,
+  children,
+  icon: Icon,
+}: {
+  className?: string
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
   )
 }
