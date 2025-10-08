fe25844320296c4568a3fd6cfa2e8ec7bba07c1c
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-10-08 15:11:48 +0200

    feat: Enhance privacy policy and terms of use pages with improved layout and content structure

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -9,6 +9,15 @@ type ProseProps = ComponentPropsWithoutRef<'div'> & {
 
 export function Prose({ className, ...props }: ProseProps) {
   return (
-    <div className={clsx(className, 'prose dark:prose-invert')} {...props} />
+    <div
+      className={clsx(
+        className,
+        'prose dark:prose-invert',
+        '[&_ol]:my-4 [&_ul]:my-4',
+        '[&_li]:my-1',
+        '[&_ol>li]:my-0.5 [&_ul>li]:my-0.5'
+      )}
+      {...props}
+    />
   )
 }
