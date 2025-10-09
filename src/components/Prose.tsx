2c010c6a9e8fee8992fe0c110f508ae1af63dd5a
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-10-09 09:05:12 +0200

    feat: Update layout and styling for privacy policy and terms of use pages

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -13,6 +13,8 @@ export function Prose({ className, ...props }: ProseProps) {
       className={clsx(
         className,
         'prose dark:prose-invert',
+        '[&_h2]:mt-8 [&_h2]:mb-4',
+        '[&_h3]:mt-6 [&_h3]:mb-3',
         '[&_ol]:my-4 [&_ul]:my-4',
         '[&_li]:my-1',
         '[&_ol>li]:my-0.5 [&_ul>li]:my-0.5'
