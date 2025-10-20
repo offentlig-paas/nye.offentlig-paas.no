be00ccccc434bd0343484899f0b694564a8e4660
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2025-10-20 22:33:13 +0200

    typography: replace '@screen lg' with computed '@media (min-width: ...)' for hr layout

--- typography.ts
+++ typography.ts
@@ -223,7 +223,7 @@ export default {
             marginBottom: theme('spacing.20'),
             borderTopWidth: '1px',
             borderColor: 'var(--tw-prose-hr)',
-            '@screen lg': {
+            [`@media (min-width: ${theme('screens.lg')})`]: {
               marginLeft: `calc(${theme('spacing.12')} * -1)`,
               marginRight: `calc(${theme('spacing.12')} * -1)`,
             },
