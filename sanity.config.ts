30c6cd3ee45c913bfa084b3071364d5f34162ab0
Hans Kristian Flaatten <hans.kristian.flaatten@nav.no>
2026-04-07 23:12:58 +0200

    fix: add type annotation for StructureBuilder in sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
+import { type StructureBuilder, structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 
 import { schemas } from './src/lib/sanity/schemas'
@@ -20,7 +20,7 @@ export default defineConfig({
 
   plugins: [
     structureTool({
-      structure: S =>
+      structure: (S: StructureBuilder) =>
         S.list()
           .title('Content')
           .items([
